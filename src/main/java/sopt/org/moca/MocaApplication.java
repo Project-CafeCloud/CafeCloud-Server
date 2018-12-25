4cf5de1cc8e435529860d3b47c3c35bf13345ffe
choisunpil <roung4119@gmail.com>
2018-12-25 20:37:55 +0900

    create server project

---
+++ src/main/java/sopt/org/moca/MocaApplication.java
@@ -0,0 +1,14 @@
+package sopt.org.moca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MocaApplication.class, args);
+    }
+
+}
+
