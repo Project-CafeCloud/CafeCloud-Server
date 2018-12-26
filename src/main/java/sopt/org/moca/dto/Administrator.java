3e6c5519f8bfceeeda4a2e778d948d67afe1e0b6
soyeon <peach404@naver.com>
2018-12-26 21:54:50 +0900

    add admin

---
+++ src/main/java/sopt/org/moca/dto/Administrator.java
@@ -0,0 +1,11 @@
+package sopt.org.moca.dto;
+
+
+import lombok.Data;
+
+@Data
+public class Administrator {
+
+    private String administrator_id;
+    private String administrator_name;
+}
--- src/main/java/sopt/org/moca/mapper/UserMapper.java
+++ src/main/java/sopt/org/moca/mapper/UserMapper.java
@@ -1,8 +1,8 @@
 package sopt.org.moca.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import sopt.org.moca.dto.User;
+import sopt.org.moca.model.UserSignUpReq;
 
 import java.util.List;
 
@@ -10,7 +10,13 @@ import java.util.List;
 public interface UserMapper {
 
     //모든 회원 리스트 조회
-    @Select("SELECT * FROM user")
+    @Select("SELECT * FROM USER")
     List<User> findAll();
+
+
+    //회원등록
+    @Insert("INSERT INTO USER(user_id,user_password,user_name) VALUES(#{UserSignUpReq.user_id}, #{UserSignUpReq.user_password},#{UserSignUpReq.user_name})")
+    void save(@Param("userSignupReq") final UserSignUpReq userSignUpReq);
+
 
 }
