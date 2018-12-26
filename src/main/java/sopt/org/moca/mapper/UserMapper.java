e9fc34d37ecd6f09bb7229de6bb880cd9040d7b1
soyeon <peach404@naver.com>
2018-12-26 20:31:33 +0900

    add gitignore

--- src/main/java/sopt/org/moca/mapper/UserMapper.java
+++ src/main/java/sopt/org/moca/mapper/UserMapper.java
@@ -1,4 +1,16 @@
 package sopt.org.moca.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import sopt.org.moca.dto.User;
+
+import java.util.List;
+
+@Mapper
 public interface UserMapper {
+
+    //모든 회원 리스트 조회
+    @Select("SELECT * FROM user")
+    List<User> findAll();
+
 }
