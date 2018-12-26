e07523ca0dc765e68393658e1e85089cef2a9746
soyeon <peach404@naver.com>
2018-12-26 18:42:59 +0900

    test commit

---
+++ src/main/java/sopt/org/moca/dto/User.java
@@ -0,0 +1,11 @@
+package sopt.org.moca.dto;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String user_id;
+    private String user_name;
+}
---
+++ src/main/java/sopt/org/moca/model/AdministratorSignUpReq.java
@@ -0,0 +1,8 @@
+package sopt.org.moca.model;
+
+public class AdministratorSignUpReq {
+
+    private String administrator_id;
+    private String administrator_password;
+
+}
