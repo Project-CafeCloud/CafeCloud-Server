8aae56d5953d2fbe98df334f816da4f2634d720f
jiyeonkim7 <jiyean9629@gmail.com>
2019-01-04 00:39:15 +0900

    update Get USER/SOCIAL FEED

--- src/main/java/sopt/org/moca/api/FeedController.java
+++ src/main/java/sopt/org/moca/api/FeedController.java
@@ -6,19 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sopt.org.moca.dto.Review;
-import sopt.org.moca.dto.ReviewImage;
-import sopt.org.moca.dto.User;
 import sopt.org.moca.model.DefaultRes;
-import sopt.org.moca.model.ReviewReq;
 import sopt.org.moca.service.ReviewService;
 import sopt.org.moca.service.UserService;
-import sopt.org.moca.utils.ResponseMessage;
-import sopt.org.moca.utils.StatusCode;
-import sopt.org.moca.utils.auth.Auth;
 import sopt.org.moca.utils.auth.JwtUtils;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 import static sopt.org.moca.model.DefaultRes.FAIL_DEFAULT_RES;
@@ -61,11 +54,7 @@ public class FeedController {
             final String feedId = user_id;
             final String userId = JwtUtils.decode(httpServletRequest.getHeader(HEADER)).getUser_id();
 
-            List<String> feedIdList = new ArrayList<>();
-            feedIdList.add(feedId);
-
-            //String feedIdList = "['" + feedId + "']";
-            DefaultRes<List<Review>> feedList = reviewService.findByUserId(feedIdList);
+            DefaultRes<List<Review>> feedList = reviewService.findByUserId(feedId,true);
 
             if(feedList.getData() != null) {
                 for (Review r : feedList.getData()) {
@@ -96,28 +85,7 @@ public class FeedController {
 
             final String userId = JwtUtils.decode(httpServletRequest.getHeader(HEADER)).getUser_id();
 
-
-            DefaultRes<List<User>> followingList = userService.findFollow(userId, true);
-
-            //String feedIdList = "[";
-
-
-            List<String> feedIdList = new ArrayList<>();
-
-            // user가 팔로우 하고 있는 사람들
-            if(followingList.getData() != null) {
-                for (User f : followingList.getData()) {
-
-                    feedIdList.add(f.getUser_id());
-                    //feedIdList += "'" + f.getUser_id() + "',";
-
-                }
-            }
-
-            //feedIdList.substring(0, feedIdList.length()-1);
-            //log.info(feedIdList);
-
-            DefaultRes<List<Review>> feedList = reviewService.findByUserId(feedIdList);
+            DefaultRes<List<Review>> feedList = reviewService.findByUserId(userId, false);
 
             if(feedList.getData() != null) {
                 for (Review r : feedList.getData()) {
