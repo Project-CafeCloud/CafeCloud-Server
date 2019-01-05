33b05f865e10d0e0267c84da98de9e2d33b28203
jiyeonkim7 <jiyean9629@gmail.com>
2019-01-05 20:30:11 +0900

    update CAFE mapper

--- src/main/java/sopt/org/moca/mapper/CafeMapper.java
+++ src/main/java/sopt/org/moca/mapper/CafeMapper.java
@@ -36,7 +36,7 @@ public interface CafeMapper {
 
     //검증 카페 상세 정보 조회(카페이름, 카페주소,총평, 평균 별점)
     @Select("select cafe_name, cafe_address, evaluated_cafe_total_evaluation, evaluated_cafe_rating " +
-            "from EVALUATED_CAFE natural join CAFE" +
+            "from EVALUATED_CAFE natural join CAFE " +
             "where cafe_id = #{cafe_id}")
     EvaluatedCafeInfo findEvaluatedCafeInfo(@Param("cafe_id")final int cafe_id);
 
@@ -44,23 +44,24 @@ public interface CafeMapper {
 
     //검증 카페  이미지 조회
     @Select("select evaluated_cafe_img_url,evaluated_cafe_main_img " +
-            "from EVALUATED_CAFE_IMG" +
+            "from EVALUATED_CAFE_IMG " +
             "where cafe_id = #{cafe_id}")
     List<EvaluatedCafeImg>findEvaluatedCafeImg(@Param("cafe_id")final int cafe_id);
 
 
 
     //검증 카페 검증 위원 평가 정보 리스트 조회
-    @Select("select barista_id,barista_name,barista_title,barista_img_url,evaluation_bean_condition,evaluation_bean_condition_comment,evaluation_roasting,evaluation_roasting_comment,evaluation_creativity,evaluation_creativity_comment,evaluation_reasonable,evaluation_reasonable_comment,evaluation_consistancy,evaluation_consistancy_comment" +
-            "from EVALUATION natural join BARISTA" +
+    @Select("select barista_id, barista_name, barista_title, barista_img_url, " +
+            "evaluation_bean_condition, evaluation_bean_condition_comment, evaluation_roasting, evaluation_roasting_comment, evaluation_creativity, evaluation_creativity_comment, evaluation_reasonable, evaluation_reasonable_comment, evaluation_consistancy, evaluation_consistancy_comment " +
+            "from EVALUATION natural join BARISTA " +
             "where cafe_id = #{cafe_id}")
     List<Evaluation>findAllEvaluation(@Param("cafe_id")final int cafe_id);
 
 
     //해당 검증 위원 평가 정보 조회
-    @Select("select barista_name,barista_title,barista_img_url,evaluation_bean_condition,evaluation_bean_condition_comment,evaluation_roasting,evaluation_roasting_comment,evaluation_creativity,evaluation_creativity_comment,evaluation_reasonable,evaluation_reasonable_comment,evaluation_consistancy,evaluation_consistancy_comment,evaluation_summary" +
-            "from EVALUATION natural join BARISTA" +
-            "where barista_id = #{barista_id} , cafe_id = #{cafe_id}")
+    @Select("select barista_name,barista_title,barista_img_url,evaluation_bean_condition,evaluation_bean_condition_comment,evaluation_roasting,evaluation_roasting_comment,evaluation_creativity,evaluation_creativity_comment,evaluation_reasonable,evaluation_reasonable_comment,evaluation_consistancy,evaluation_consistancy_comment,evaluation_summary " +
+            "from EVALUATION natural join BARISTA " +
+            "where barista_id = #{barista_id} AND cafe_id = #{cafe_id}")
     Evaluation_detail findBaristaEvaluation(@Param("cafe_id")final int cafe_id,@Param("barista_id")final int barista_id);
 
 /**
@@ -94,8 +95,8 @@ public interface CafeMapper {
 
 
     //시그니처 메뉴 리스트 조회
-    @Select("select cafe_signiture_menu,cafe_signiture_price,cafe_signiture_img" +
-            "from CAFE_SIGNITURE" +
+    @Select("select cafe_signiture_menu,cafe_signiture_price,cafe_signiture_img " +
+            "from CAFE_SIGNITURE " +
             "where cafe_id = #{cafe_id}")
     List<CafeSignitureMenu> findCafeSigitureMenuList(@Param("cafe_id")final int cafe_id);
 
@@ -105,7 +106,7 @@ public interface CafeMapper {
      */
 
     //해당 지역구 전체
-    @Select("select cafe_id, cafe_name, cafe_introduction , cafe_latitude, cafe_longitude ,cafe_rating_avg,cafe_img_url " +
+    @Select("select cafe_id, cafe_name, cafe_introduction, cafe_latitude, cafe_longitude ,cafe_rating_avg,cafe_img_url " +
             "from CAFE natural join CAFE_IMG " +
             "where cafe_img_main = 1 and cafe_address_district_id = #{address_district_id}")
     List<CafeSimple> findCafeInfoList(@Param("address_district_id")final int address_district_id);
