2769056b8f818424a9d0113a8ad8a50beabbd90a
soyeon <peach404@naver.com>
2019-01-07 03:50:28 +0900

    fixed map

--- src/main/java/sopt/org/moca/mapper/MessageMapper.java
+++ src/main/java/sopt/org/moca/mapper/MessageMapper.java
@@ -4,7 +4,11 @@ package sopt.org.moca.mapper;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import sopt.org.moca.dto.Message;
 import sopt.org.moca.model.MessageReq;
+
+import java.util.List;
 
 @Mapper
 public interface MessageMapper {
@@ -13,9 +17,12 @@ public interface MessageMapper {
      *
      * 쪽지 보내기
      * **/
-    @Insert("INSERT INTO MESSAGE(sender_id,receiver_id,message_content,message_send_date,message_img_url) VALUES(#{messageReq.sender_id},#{messageReq.receiver_id}," +
+
+    @Insert("INSERT INTO MESSAGE(sender_id,receiver_id,message_content,message_send_date,message_img) VALUES(#{messageReq.sender_id},#{messageReq.receiver_id}," +
             "#{messageReq.message_content},#{messageReq.message_send_date},#{messageReq.message_img_url})")
     void save(@Param("messageReq") final MessageReq messageReq);
 
 
+    @Select("SELECT * FROM MESSAGE ")
+    List<Message> findByUserId(@Param("user_id") final String user_id);
 }
--- src/main/java/sopt/org/moca/service/MessageService.java
+++ src/main/java/sopt/org/moca/service/MessageService.java
@@ -1,9 +1,14 @@
 package sopt.org.moca.service;
 
+import sopt.org.moca.dto.Message;
 import sopt.org.moca.model.DefaultRes;
 import sopt.org.moca.model.MessageReq;
+
+import java.util.List;
 
 public interface MessageService {
 
     DefaultRes save(final MessageReq messageReq);
+//    DefaultRes<List<Message>> findMessageList(final String user_id);
+
 }
