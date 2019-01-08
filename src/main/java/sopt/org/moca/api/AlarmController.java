464941745a3b4f9235eaafc3712bd4477eabec88
soyeon <peach404@naver.com>
2019-01-08 23:34:00 +0900

    9차 배포

--- src/main/java/sopt/org/moca/api/AlarmController.java
+++ src/main/java/sopt/org/moca/api/AlarmController.java
@@ -1,32 +1,32 @@
-package sopt.org.moca.api;
-
-import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import sopt.org.moca.service.AndroidPushNotificationsService;
-
-@Slf4j
-@RestController
-@RequestMapping("/pushalarm")
-public class AlarmController {
-    private final AndroidPushNotificationsService androidPushNotificationsService;
-
-    public AlarmController(final AndroidPushNotificationsService androidPushNotificationsService) {
-        this.androidPushNotificationsService = androidPushNotificationsService;
-    }
-
-    @GetMapping("")
-    public ResponseEntity<String> send() {
-        JSONObject notification = new JSONObject();
-        JSONArray array = new JSONArray(); //
-
-        notification.put("registration_ids",array);
-
-    }
-}
+//package sopt.org.moca.api;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//import sopt.org.moca.service.AndroidPushNotificationsService;
+//
+//@Slf4j
+//@RestController
+//@RequestMapping("/pushalarm")
+//public class AlarmController {
+//    private final AndroidPushNotificationsService androidPushNotificationsService;
+//
+//    public AlarmController(final AndroidPushNotificationsService androidPushNotificationsService) {
+//        this.androidPushNotificationsService = androidPushNotificationsService;
+//    }
+//
+//    @GetMapping("")
+//    public ResponseEntity<String> send() {
+//        JSONObject notification = new JSONObject();
+//        JSONArray array = new JSONArray(); //
+//
+//        notification.put("registration_ids",array);
+//
+//    }
+//}
